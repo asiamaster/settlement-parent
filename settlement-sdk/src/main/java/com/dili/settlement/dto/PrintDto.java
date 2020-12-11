9be5018e09b9328229a57c2925b5c4741264ebf3
xuliang <514132096@qq.com>
2020-12-11 10:15:59 +0800

    增加打印DTO；

---
+++ settlement-sdk/src/main/java/com/dili/settlement/dto/PrintDto.java
@@ -0,0 +1,33 @@
+package com.dili.settlement.dto;
+
+/**
+ * 打印传输对象
+ */
+public class PrintDto {
+
+    //打印模板名称
+    private String name;
+    //打印数据 由业务系统和客户端沟通
+    private Object item;
+
+    public PrintDto(String name, Object item){
+        this.name = name;
+        this.item = item;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Object getItem() {
+        return item;
+    }
+
+    public void setItem(Object item) {
+        this.item = item;
+    }
+}
